9e0d7f9a944ce37455e2aa2adf064b7e8a720586
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-08 12:56:52 +0800

    create darkmode functionality

--- src/DarkMode.jsx
+++ src/DarkMode.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useGlobalContext } from "./context";
+import {FaSun, FaRegMoon} from 'react-icons/fa'
 
 export default function DarkMode() {
+  const { isDarkTheme, toggleDarkTheme } = useGlobalContext();
   return (
-    <div>
-      DarkMode
-    </div>
-  )
+    <section className="toggle-container max-w-var(--max-width) mx-auto py-4 px-0 flex justify-end">
+      <button
+        className="dark-toggle bg-transparent border-transparent w-20 h-8 grid place-items-center cursor-pointer"
+        onClick={toggleDarkTheme}
+      >{isDarkTheme ? (
+        <FaSun className="text-yellow-500 text-2xl" />
+
+      ) : (
+        <FaRegMoon className="toggle-icon text-violet-700 text-2xl" />
+      )}</button>
+    </section>
+  );
 }
