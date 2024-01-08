19f0a93137d8d237af92af47a8def9147fd5a31a
John Carlo Lipa <johncarlolipa@gmail.com>
2024-01-08 12:22:16 +0800

    set-up context

---
+++ src/context.js
@@ -0,0 +1,33 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const savedTheme = localStorage.getItem("theme");
+  const [isDarkTheme, setIsDarkTheme] = useState(savedTheme === "dark");
+  const [searchTerm, setSearchTerm] = useState("nature");
+
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+    const body = document.querySelector("body");
+    body.classList.toggle("dark", newDarkTheme);
+
+    localStorage.setItem("theme", newDarkTheme ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    const body = document.querySelector("body");
+    body.classList.toggle("dark", isDarkTheme);
+  }, [isDarkTheme]);
+
+  return (
+    <AppContext.Provider
+      value={{ isDarkTheme, toggleDarkTheme, searchTerm, setSearchTerm }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => useContext(AppContext);
